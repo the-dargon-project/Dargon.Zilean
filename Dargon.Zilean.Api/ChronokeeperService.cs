8a80d0a7eb29b61314517d423a76e895b668a281
Warty <ItzWarty@gmail.com>
2015-09-12 00:34:21 -0700

    ChronokeeperService exposes methods for getting multiple ids.

--- Dargon.Zilean.Api/ChronokeeperService.cs
+++ Dargon.Zilean.Api/ChronokeeperService.cs
@@ -3,7 +3,10 @@ using System.Runtime.InteropServices;
 
 namespace Dargon.Zilean {
    [Guid("7C5DF5D6-9251-4F4D-A5BA-235CEEE8C300")]
-   public interface ChronokeeperService : Chronokeeper {}
+   public interface ChronokeeperService : Chronokeeper {
+      long[] GenerateSequentialIds(int count);
+      Guid[] GenerateSequentialGuids(int count);
+   }
 
    public interface Chronokeeper {
       long GenerateSequentialId();
